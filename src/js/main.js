41775373fc97831ba75ad76ab02cef9c63ace75e
= <=>
2024-02-10 17:26:53 +0530

    services section done

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,24 @@ navToggler.addEventListener('click', () => {
     }
 });
 /*------------------------------- Slider--------------------------------*/
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+const header = document.querySelector('header')
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY >= 600) {
+        header.classList.add('sticky')
+    } else {
+        header.classList.remove('sticky')
+    }
+})
